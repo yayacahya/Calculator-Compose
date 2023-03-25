3f0137bfaf0e3313e3f0a466cfef5f803cf43f8d
Yaya Nurcahya <v-yaya.nurcahya@anteraja.id>
2023-03-25 21:22:14 +0700

    fixed AutoResizedText

--- app/src/main/java/com/example/calculator_compose/AutoResizedText.kt
+++ app/src/main/java/com/example/calculator_compose/AutoResizedText.kt
@@ -9,6 +9,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.TextUnit
 
 @Composable
 fun AutoResizedText(
@@ -27,7 +28,13 @@ fun AutoResizedText(
         mutableStateOf(false)
     }
 
-//    val defaultFontSize = MaterialTheme.typography.h1.fontSize
+    val defaultFontSize = MaterialTheme.typography.h1.fontSize
+
+    var startFontSizeChanged by remember {
+        mutableStateOf(0)
+    }
+
+    val lengthAndFontSizeMap = remember { mutableStateMapOf<Int, TextUnit>() }
 
     Text(
         text = text,
@@ -44,16 +51,37 @@ fun AutoResizedText(
         maxLines = maxLines,
         onTextLayout = { result ->
             if (result.didOverflowWidth) {
-//                if (style.fontSize.isUnspecified) {
-//                    resizedTextStyle = resizedTextStyle.copy(
-//                        fontSize = defaultFontSize
-//                    )
-//                    Log.i("defaultFontSize xxx", "$defaultFontSize")
-//                }
+                lengthAndFontSizeMap.filterKeys { it == text.length }
                 resizedTextStyle = resizedTextStyle.copy(
                     fontSize = resizedTextStyle.fontSize * 0.95
                 )
+
+                if (startFontSizeChanged == 0) {
+                    startFontSizeChanged = text.length
+
+                    lengthAndFontSizeMap[startFontSizeChanged] = resizedTextStyle.fontSize
+                } else {
+                    if (text.length >= startFontSizeChanged) {
+                        lengthAndFontSizeMap[text.length] = resizedTextStyle.fontSize
+                    }
+                }
             } else {
+                if (startFontSizeChanged > 0) {
+                    resizedTextStyle = when (text.length) {
+                        in 1 until startFontSizeChanged -> {
+                            resizedTextStyle.copy(
+                                fontSize = defaultFontSize
+                            )
+                        }
+                        0 -> return@Text
+                        else -> {
+                            resizedTextStyle.copy(
+                                fontSize = lengthAndFontSizeMap[text.length]
+                                    ?: resizedTextStyle.fontSize
+                            )
+                        }
+                    }
+                }
                 shouldDraw = true
             }
         }
